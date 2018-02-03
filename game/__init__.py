862cfc26ffa8a65f44ede6a40346492e2f2ae98a
Kamil S <hohlik741@gmail.com>
2018-02-03 11:39:59 +0100

    Easy framework

---
+++ game/__init__.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Snake")
+
+
+pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
+
+pygame.quit()
+quit()
+
+
